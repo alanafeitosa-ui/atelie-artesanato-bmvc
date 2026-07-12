34c3b9480a093a717e265fcdb137dc87fe85498d
Alana Cristyna Feitosa Dias <alanafeitosa84996@gmail.com>
2026-07-12 20:41:49 -0300

    feat: add sqlite database initialization

---
+++ src/database/connection.py
@@ -0,0 +1,9 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_PATH = os.path.join(BASE_DIR, "bmvc.db")
+
+def get_connection():
+    connection = sqlite3.connect(DATABASE_PATH)
+    return connection
